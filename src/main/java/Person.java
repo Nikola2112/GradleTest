bd953d3bfe903014c49ef901e568795ebacd8d00
Nikola2112 <nikola21122004@gmail.com>
2023-06-07 15:36:19 +0100

    new

---
+++ src/main/java/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+    private String name;
+    private String LastName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public Person(String name, String lastName) {
+        this.name = name;
+        LastName = lastName;
+    }
+}
